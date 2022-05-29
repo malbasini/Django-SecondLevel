32e5c746496fc3a18dbea2727cd3af45601a88d7
malbasini <malbasini@gmail.com>
2022-05-29 15:47:32 +0200

    Commit

--- DjangoSecondLevel/news/views.py
+++ DjangoSecondLevel/news/views.py
@@ -3,21 +3,6 @@ from django.shortcuts import render,get_object_or_404
 from django.http import HttpResponse
 from .models import Articolo,Giornalista
 # Create your views here.
-#def home(request):
-#   return HttpResponse('<h1> Homepage! </h1>')
-
-""" def home(request):
-    a = ""
-    g = ""
-    for art in Articolo.objects.all():
-        a += (art.titolo +'<br>')
-    for gio in Giornalista.objects.all():
-        g += (gio.nome +'<br>')
-    response = 'Articoli: <br>' + a + '<br> Giornalisti: <br>' + g
-    return HttpResponse('<h1>' + response + '</h1>') """
-    
-    #UTILIZZO DEI TEMPLATE
-    
 def home(request):
     articoli = Articolo.objects.all()
     giornalisti = Giornalista.objects.all()
@@ -26,6 +11,10 @@ def home(request):
 
 def articoloDetailView(request,pk):
     #articolo = Articolo.objects.get(pk=pk)
+    #get_object_or_404 restituisce l'articolo se
+    #esiste o una schermata con codice 404 (not found)
+    #se l'articolo non esiste. Decisamente meglio
+    #di una schermata di errore.
     articolo = get_object_or_404(Articolo,pk=pk)
     context={'articolo':articolo}
     return render(request,'articoloDetail.html',context)
